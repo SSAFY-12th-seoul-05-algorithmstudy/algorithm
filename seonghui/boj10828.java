a3c3dd737f4a02e982bacfa10239b79102cebd58
LeeSeongHui <seonghui3472@gmail.com>
2024-07-31 02:21:59 +0900

    [solved] boj10828

--- seonghui/boj10828.java
+++ seonghui/boj10828.java
@@ -19,32 +19,28 @@ public class boj10828 {
 
 			String s = br.readLine(); // 명령어 입력
 
-			if (s.contains("push")) { //(1) push X: 정수 X를 스택에 넣는 연산이다.
+			if (s.contains("push")) { // (1) push X: 정수 X를 스택에 넣는 연산이다.
 				String[] command = s.split(" "); // [push, 1]
 				stack[++pos] = Integer.parseInt(command[1]);
-				
-				
-			} else if (s.equals("pop")) { //(2) pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+			} else if (s.equals("pop")) { // (2) pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 				if (pos == -1) {
 					System.out.println("-1");
 				} else {
 					System.out.println(stack[pos--]);
 				}
-				
-				
-			} else if(s.equals("size")) { //(3) size: 스택에 들어있는 정수의 개수를 출력한다.
-				System.out.println(pos+1); // error => pos 초기값 -1이므로 pos+1이 되야 현재 들어있는 정수 개수가 됨
-				
-				
-			} else if(s.equals("empty")) { //(4) empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
+
+			} else if (s.equals("size")) { // (3) size: 스택에 들어있는 정수의 개수를 출력한다.
+				System.out.println(pos + 1); // error => pos 초기값 -1이므로 pos+1이 되야 현재 들어있는 정수 개수가 됨
+
+			} else if (s.equals("empty")) { // (4) empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
 				if (pos == -1) {
 					System.out.println("1");
 				} else {
 					System.out.println("0");
 				}
-				
-				
-			} else if(s.equals("top")) { //(5) top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+			} else if (s.equals("top")) { // (5) top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 				if (pos == -1) {
 					System.out.println("-1");
 				} else {
