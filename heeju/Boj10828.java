92ca30229861d3b7f686d9a1c4797757da511a24
HeeJu Cho <sus6an@naver.com>
2024-07-31 10:57:26 +0900

    [fix] 파일명 변경

--- heeju/Boj10828.java
+++ heeju/Boj10828.java
@@ -6,7 +6,7 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Boj10828 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
