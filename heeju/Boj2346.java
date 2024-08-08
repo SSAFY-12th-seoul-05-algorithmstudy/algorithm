2ebb6f3cbf3e80cd1ba9c97869a950cdac6a59d5
HeeJu Cho <sus6an@naver.com>
2024-08-08 14:55:00 +0900

    [Update] 주석 추가

--- heeju/Boj2346.java
+++ heeju/Boj2346.java
@@ -23,8 +23,8 @@ public class Boj2346 {
 
 		// 2. 계산
 		StringBuilder sb = new StringBuilder();
-		int idx = 0;
-		int add = 1;
+		int idx = 0;  // 터트릴 풍선 인덱스
+		int add = 1;  // 이미 터진 풍선일때, 추가로 이동해줄 값
 
 		for (int i = 0; i < n; i++) {
 			// 2.1 음수인지 양수인지 확인
@@ -36,12 +36,12 @@ public class Boj2346 {
 			sb.append(idx + 1).append(" ");
 
 			// 2.3 이동 및 유효성 확인
-			if (i == n - 1) {
+			if (i == n - 1) {     // 마지막 풍선이면 이동 안함
 				break;
 			}
-			while (tmp != 0) {
-				idx = move(n, idx + add);
-				if (arr[idx] != 0) {
+			while (tmp != 0) {    // 한 칸씩 이동하면서 터지지 않은 풍선 찾기
+				idx = move(n, idx + add);   // -1 or 1 이동 후, 유효한 인덱스로 변경
+				if (arr[idx] != 0) {        // 해당 인덱스의 숫자가 0이면(이미 터진 풍선) -> add방향으로 추가 이동
 					tmp -= add;
 				}
 			}
@@ -55,6 +55,7 @@ public class Boj2346 {
 	}
 
 	static int move(int n, int idx) {
-		return idx >= 0 ? idx % n : n + (idx % n);
+		return idx >= 0 ? idx % n : n + (idx % n); // 인덱스가 0보다 작으면, 유효한 인덱스로 변경
+		                                           // ex) idx = -4; 고 n = 3이면, -> 2로 변경됨
 	}
 }
