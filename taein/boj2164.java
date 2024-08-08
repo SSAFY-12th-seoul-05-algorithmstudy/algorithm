9d47fb4a44c86a6a06eadd4cba438e8a1f4d94e9
Kim-Taein <103629213+Kim-Taein@users.noreply.github.com>
2024-08-09 08:40:38 +0900

    3조 김태인/ 8월 2주차/ 목 (#37)
    * [solved] boj1874.java
    
    * [solved] boj9012java
    
    * [solved] boj10773.java
    
    * [solved] boj1874.java
    
    * [solved]boj9012.java
    
    * [solved] boj11866.java
    
    * [solved] boj1158.java
    
    * [solved] boj1158.java
    
    * [solved] boj2346.java
    
    * [solved] boj2164.java
    
    * [solved] boj2346.java
    
    ---------
    
    Co-authored-by: @yam3472 <yam3472@naver.com>

---
+++ taein/boj2164.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class boj2164 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Queue<Integer> q = new LinkedList<>();
+
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=N; i++) {
+			q.add(i); // 1부터 N까지 수를 queue에 add
+		}
+		
+		while(q.size() != 1) {
+			q.poll(); // 제일 위에 있는 카드 버림
+			q.add(q.poll()); // 그 다음 위에 있는 카드를 제일 아래로
+		}
+		
+		System.out.println(q.poll());
+	}
+}
