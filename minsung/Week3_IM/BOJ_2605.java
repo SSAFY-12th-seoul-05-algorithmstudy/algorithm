90d9a45910f484d7529cd939b09f791c4454cf52
MINSSUNGKIM <planman0@gmail.com>
2024-08-15 08:56:21 +0900

    [solved] boj10163

---
+++ minsung/Week3_IM/BOJ_2605.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		List<Integer> students = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+
+			if (students.isEmpty()) {
+				students.add(i + 1);
+				continue;
+			}
+			students.add(students.size() - arr[i], i + 1);
+
+		}
+
+		for (int i = 0; i < N; i++) {
+			System.out.print(students.get(i)+" ");
+
+		}
+
+	}
+
+}
