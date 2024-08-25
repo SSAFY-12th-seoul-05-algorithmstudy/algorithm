d7551a7b1a1be4c6efcfa76124ab2308d842fb7e
ximvamom <140465587+ximvamom@users.noreply.github.com>
2024-08-25 14:14:08 +0900

    1조 김윤지 / 8월 3주차 / 목 (#53)
    * [solved]boj10828
    
    * [solved]Boj_3986
    
    * [solved] boj10773
    
    * [solved] boj9012
    
    * [solved] boj1158
    
    * [solved] boj2346
    
    * [solved] boj1021
    
    * [solved] boj2309
    
    * [solved] boj24060

---
+++ yoonji/BOJ_25501.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BOJ_25501{
+	
+    public static int recursion(String s, int l, int r){
+    	cnt++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    
+    
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+    
+    static int cnt;
+    
+    public static void main(String[] args){
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	
+    	for(int i=0; i<n; i++) {
+    		cnt = 0;
+    		String s = sc.next();
+    		System.out.printf("%d %d\n", isPalindrome(s), cnt);
+    		
+    	}
+    }
+}
