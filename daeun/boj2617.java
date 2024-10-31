040eb812cd097ce96da00ae3b255df1752f112ea
daeun <daeunchang@naver.com>
2024-10-31 17:47:54 +0900

    [solved]boj2617

---
+++ daeun/boj2617.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class 구슬찾기 {
+	
+	
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int N = sc.nextInt(); ///구슬 개수
+		int M = sc.nextInt();//간선 느낌
+		//앞번호 구슬이 뒤꺼볻 무거움 -> 앞>뒤
+		ArrayList<Integer>[] graph= new ArrayList[N+1];
+		
+		
+		
+	}//main
+
+}
